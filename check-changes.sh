62d9e42f7e794c5732a60edc9a36ba412c3cabd1
Oliver Butterley <oliver.butterley@gmail.com>
2024-05-14 11:04:27 +0200

    🎨 refactor mathlib check

--- check-changes.sh
+++ check-changes.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-# Check if either "lean-toolchain" or "lake-manifest.json" have been modified, 
+# Check if either "lean-toolchain" or "lake-manifest.json" have been modified,
 # ignoring white space changes.
 
 if [[ -n $(git diff -w "lean-toolchain") ]] || [[ -n $(git diff -w "lake-manifest.json") ]]; then
-    echo "files_changed=true";
+    echo "files_changed=true"
 else
-    echo "files_changed=false";
+    echo "files_changed=false"
 fi
