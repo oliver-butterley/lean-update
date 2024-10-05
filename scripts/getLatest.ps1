a701e16535d74173326d7a162e5a1cbe2d2b235b
Kitamado <47292598+Seasawher@users.noreply.github.com>
2024-10-06 07:32:16 +0900

    Exception calling ".ctor" with "1" argument(s): (#38)

--- scripts/getLatest.ps1
+++ scripts/getLatest.ps1
@@ -1,17 +1,20 @@
 # get all release tags from leanprover/lean4
+# and filter out the ones that are not form of `v*`
 $versions = gh release list `
   --repo leanprover/lean4 `
   --json tagName | `
   ConvertFrom-Json | `
   ForEach-Object { $_.tagName } | `
-  ForEach-Object { $_ -replace '^v' }
+  Where-Object { $_ -like 'v*' }
 
 # parse the version tags as semver
-$semvers = $versions | ForEach-Object { [System.Management.Automation.SemanticVersion]::new($_) }
+$semvers = $versions | `
+  ForEach-Object { $_ -replace '^v' } | `
+  ForEach-Object { [System.Management.Automation.SemanticVersion]::new($_) }
 
 # sort the versions and get the latest one
 $latest = $semvers | Sort-Object | Select-Object -Last 1
-Write-Host "Latest Lean release is: $latest"
+Write-Host "Latest Lean release is: v$latest"
 
 # update `lean-toolchain` file
 $leanStyleVersion = "leanprover/lean4:v$latest"
